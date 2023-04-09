1e5333bfc59719c7b1b44fbb0a8d9a2e456a59c1
rahul01-git <rahulneupane0090@gmail.com>
2023-04-09 12:51:27 +0545

    setting gameplay

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,5 +1,28 @@
-public class GamePlay {
-    GamePlay(){
-        
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePlay extends JPanel {
+    private ImageIcon titleImage;
+
+    public GamePlay(){
+
     }
+
+    public void paint(Graphics g){
+        //display title image
+        titleImage = new ImageIcon("title.png");
+        titleImage.paintIcon(this,g,25,5);
+
+        //display gameplay borders
+        g.setColor(Color.DARK_GRAY);
+        g.drawRect(24,74,851,577);
+
+        //display gameplay background
+        g.setColor(Color.black);
+        g.fillRect(25,75,851,577);
+
+        g.dispose();
+    }
+
+
 }
